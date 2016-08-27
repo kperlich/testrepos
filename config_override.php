e064c027347502503114506c5ab2f52c2f8757b4
Karsten Perlich <Karsten Perlich>
2016-08-27 15:55:17 +0200

    Accounts DOM aufgeräumt

--- config_override.php
+++ config_override.php
@@ -7,4 +7,5 @@ $sugar_config['default_date_format'] = 'd.m.Y';
 $sugar_config['default_decimal_seperator'] = ',';
 $sugar_config['default_number_grouping_seperator'] = '.';
 $sugar_config['default_time_format'] = 'H:i';
+$sugar_config['default_language'] = 'de_de';
 /***CONFIGURATOR***/
